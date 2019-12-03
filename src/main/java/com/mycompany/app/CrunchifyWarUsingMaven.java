75153fb9a5ddf2bfc33f3a71d943a9e10811c628
senthiljack <senthil_94@yahoo.com>
2019-12-03 19:10:56 +0530

    Update and rename A.java to CrunchifyWarUsingMaven.java

---
+++ src/main/java/com/mycompany/app/CrunchifyWarUsingMaven.java
@@ -0,0 +1,14 @@
+
+package crunchify.com.tutorials;
+ 
+/**
+ * @author Crunchify.com
+ *
+ */
+ 
+public class CrunchifyWarUsingMaven {
+	public static void main(String[] args) {
+		System.out
+				.println("Test.. Test by Crunchify.. \nThis is a simple tutorial on how to create .war file using Maven Plugin..");
+	}
+}
